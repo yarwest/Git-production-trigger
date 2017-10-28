c9ece9304293519bd4976a22c3c394d0e1a2b0fc
yarwest <yarnoboelens@gmail.com>
2017-10-28 23:50:18 +0200

    The first commit now gets stripped down to its hash.

--- trigger.scala
+++ trigger.scala
@@ -1,5 +1,10 @@
 val doc = scala.io.Source.fromURL("https://github.com/yarwest/Git-production-trigger/commits/master", "UTF-8")
 val commitString: String = ".*class=\"message\".*"
-println(doc.getLines.filter(line => line.matches(commitString)).next)
+
+retrieveCommitHash(doc.getLines.filter(line => line.matches(commitString)).next)
 
 doc.close
+
+def retrieveCommitHash(str : String) : String = {
+    return str.replaceFirst("</a>", "").replaceFirst("^.*href=\".*/", "").replaceFirst("\".*$", "")
+}
