8b45b95ef661ac72b475446e4922de34cf2396a4
yarwest <yarnoboelens@gmail.com>
2017-10-29 00:18:01 +0200

    Check if the remote repo is at the same commit as the local repo and output an apropriate message.

--- trigger.scala
+++ trigger.scala
@@ -1,9 +1,17 @@
 val doc = scala.io.Source.fromURL("https://github.com/yarwest/Git-production-trigger/commits/master", "UTF-8")
 val commitString: String = ".*class=\"message\".*"
 
-retrieveCommitHash(doc.getLines.filter(line => line.matches(commitString)).next)
+val latestHash = retrieveCommitHash(doc.getLines.filter(line => line.matches(commitString)).next)
 
 doc.close
+
+val currentHash = scala.io.Source.fromFile(".git/refs/heads/master").getLines.next
+
+if(!currentHash.equals(latestHash)) {
+    println("It seems the remote repository is ahead of the local repository. Pulling to retrieve the changes.")
+} else {
+    println("The local branch is up to date.")
+}
 
 def retrieveCommitHash(str : String) : String = {
     return str.replaceFirst("</a>", "").replaceFirst("^.*href=\".*/", "").replaceFirst("\".*$", "")
