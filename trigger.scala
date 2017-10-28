830feb09fbc7b5a41e6f2e1c2300a051e5ecc6bb
yarwest <yarnoboelens@gmail.com>
2017-10-28 22:30:29 +0200

    Made it that only the element containing the first commit is printed.

--- trigger.scala
+++ trigger.scala
@@ -1,1 +1,5 @@
-println(scala.io.Source.fromURL("https://github.com/yarwest/Git-production-trigger/commits/master").mkString);
+val doc = scala.io.Source.fromURL("https://github.com/yarwest/Git-production-trigger/commits/master", "UTF-8")
+val commitString: String = ".*class=\"message\".*"
+println(doc.getLines.filter(line => line.matches(commitString)).next)
+
+doc.close
