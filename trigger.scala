14b530e8fdcf8e9a336d4ee04e4e1b7c1c2616b4
yarwest <yarnoboelens@gmail.com>
2017-10-29 20:22:16 +0100

    Refactored code into a function for increased readability, pretified console output while I was at it.

--- trigger.scala
+++ trigger.scala
@@ -1,23 +1,27 @@
 import sys.process._
 
-val repo = scala.io.Source.fromFile(".git/config").getLines.filter(line => line.matches("^\turl =.*")).mkString.stripPrefix("\turl = git@github.com:")
+val repo = getRepo
 
-val doc = scala.io.Source.fromURL("https://github.com/" + repo + "/commits/master", "UTF-8")
-val commitString: String = ".*class=\"message\".*"
-
-val latestHash = retrieveCommitHash(doc.getLines.filter(line => line.matches(commitString)).next)
-
-doc.close
+val latestHash = retrieveCommitHash
 
 val currentHash = scala.io.Source.fromFile(".git/refs/heads/master").getLines.next
 
 if(!currentHash.equals(latestHash)) {
-    println("It seems the remote repository is ahead of the local repository. Pulling to retrieve the changes.")
+    println("It seems the remote repository is ahead of the local repository. Pulling to retrieve the changes.\n----------")
     if("git pull".! != 0) {
-        println("There was a problem while updating the repository.")
+        println("----------\nThere was a problem while updating the repository.")
     }
 }
 
-def retrieveCommitHash(str : String) : String = {
-    return str.replaceFirst("</a>", "").replaceFirst("^.*href=\".*/", "").replaceFirst("\".*$", "")
+def getRepo() : String = {
+    return scala.io.Source.fromFile(".git/config").getLines.filter(line => line.matches("^\turl =.*")).mkString.stripPrefix("\turl = git@github.com:")
+}
+
+def retrieveCommitHash() : String = {
+    val doc = scala.io.Source.fromURL("https://github.com/" + repo + "/commits/master", "UTF-8")
+    val returnVal = doc.getLines.filter(line => line.matches(".*class=\"message\".*")).next.replaceFirst("</a>", "").replaceFirst("^.*href=\".*/", "").replaceFirst("\".*$", "")
+
+    doc.close
+
+    return returnVal
 }
